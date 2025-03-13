1d360c0305d514223a53ce1da40a725104e193a9
sudharshan-n-g <152157391+sudharshan-n-g@users.noreply.github.com>
2025-03-13 19:29:39 +0530

    admin updated code

--- admin.py
+++ admin.py
@@ -2,6 +2,7 @@ import json
 from pymongo import MongoClient
 from mail import review_performance
 from werkzeug.security import check_password_hash
+from datetime import datetime
 
 def add_new_user(user_input):
     client = MongoClient("mongodb+srv://prashitar:Vision123@cluster0.v7ckx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -15,17 +16,21 @@ def delete_emp(emp_name):
     collection = db["Employee_data"]
     collection_AM =db["Employee_AM"]
     collection_PM =db["Employee_PM"]
-    result = collection.delete_one({"employee_name": emp_name})
+    collection.delete_one({"email": emp_name})
     collection_AM.delete_many({"employee_name": emp_name})
     collection_PM.delete_many({"employee_name": emp_name})
 
 def get_emp_data(emp_name,date):
+    try:
+        formatted_date = datetime.strptime(date, "%m-%d-%Y").strftime("%Y-%m-%d")
+    except ValueError:
+        return {"error": "Invalid date format. Use MM-DD-YYYY."}
     client = MongoClient("mongodb+srv://prashitar:Vision123@cluster0.v7ckx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     db = client["Timesheet"]
     collection = db["Employee_PM"]
     collection_AM = db["Employee_AM"]
-    emp_data_PM = collection.find_one({"employee_name": emp_name, "date": date},{"_id":0})
-    emp_data_AM = collection_AM.find_one({"employee_name": emp_name, "date": date},{"_id":0})
+    emp_data_PM = collection.find_one({"employee_name": emp_name, "date": formatted_date},{"_id":0})
+    emp_data_AM = collection_AM.find_one({"employee_name": emp_name, "date": formatted_date},{"_id":0})
     emp_data = { "PM": emp_data_PM, "AM": emp_data_AM }
     return emp_data
 
