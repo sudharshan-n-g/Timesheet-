22a2b57134b8d014ba9e1c2e4cc593c341d65415
sudharshan-n-g <152157391+sudharshan-n-g@users.noreply.github.com>
2025-03-13 19:13:51 +0530

    updated admin code

--- admin.py
+++ admin.py
@@ -23,7 +23,10 @@ def get_emp_data(emp_name,date):
     client = MongoClient("mongodb+srv://prashitar:Vision123@cluster0.v7ckx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     db = client["Timesheet"]
     collection = db["Employee_PM"]
-    emp_data = collection.find_one({"employee_name": emp_name, "date": date},{"_id":0})
+    collection_AM = db["Employee_AM"]
+    emp_data_PM = collection.find_one({"employee_name": emp_name, "date": date},{"_id":0})
+    emp_data_AM = collection_AM.find_one({"employee_name": emp_name, "date": date},{"_id":0})
+    emp_data = { "PM": emp_data_PM, "AM": emp_data_AM }
     return emp_data
 
 
